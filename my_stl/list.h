26824869794af6fa40cccb59ea080f8456bfc104
huichuan <lhc14124908@163.com>
2022-06-05 15:01:18 +0800

    add

--- my_stl/list.h
+++ my_stl/list.h
@@ -7,6 +7,9 @@
 #include <cstddef>
 #include <cstdlib>
 #include <assert.h>
+#include <iostream>
+using namespace std;
+
 template<class _Ty>
 class list {
  public:
@@ -27,29 +30,162 @@ class list {
   };
 
   typedef size_t size_type;
-  typedef _Ty value_type;
-  typedef _Ty *pointer_type;
-  typedef _Ty &reference_type;
-  typedef const _Ty *const_pointer_type;
-  typedef const _Ty &const_reference_type;
+  typedef _Ty value;
+  typedef _Ty *pointer;
+  typedef _Ty &reference;
+  typedef const _Ty *const_pointer;
+  typedef const _Ty &const_reference;
   typedef int difference_type;
+  typedef const_iterator _It;
+  class const_iterator {
+   public:
+    const_iterator() {}
+    const_iterator(_Nodeptr _P) : _Ptr(_P) {}
+    const_iterator(const_iterator &_X) : _Ptr(_X._Ptr) {}
+    const_reference operator*() const { return _Acc::_Value(_Ptr); }
+    //++it   it++
+    const_iterator operator++() {
+      _Ptr = _Acc::_Next(_Ptr); // _Ptr = _Ptr->_Next;
+      return *this;
+    }
+    const_iterator operator++(int) {
+      const_iterator _Tmp = *this;
+      ++*this;
+      return _Tmp;
+    }
+    const_iterator operator--() {
+      _Ptr = _Acc::_Prev(_Ptr);
+      return *this;
+    }
+    const_iterator operator--(int) {
+      const_iterator _Tmp = *this;
+      --*this;
+      return _Tmp;
+    }
+    bool operator==(const const_iterator &_X) const { return _Ptr == _X._Ptr; }
+    bool operator!=(const const_iterator &_X) const { return !(*this == _X); }
+
+    _Nodeptr _Mynode() const { return _Ptr; }
+   protected:
+    _Nodeptr _Ptr;
+  };
+
+  class iterator : public const_iterator {
+   public:
+    iterator() {}
+
+    iterator(_Nodeptr _P) {
+      this->_Ptr = _P;
+    }
+    iterator(const iterator const &next) {
+      this->_Ptr = next._Ptr;
+    }
+    reference operator*() { return _Acc::_Value(this->_Ptr); }
+
+    //++it   it++
+    iterator operator++() {
+      this->_Ptr = _Acc::_Next(this->_Ptr); // _Ptr = _Ptr->_Next;
+      return *this;
+    }
+    iterator operator++(int) {
+      iterator _Tmp = *this;
+      ++*this;
+      return _Tmp;
+    }
+    iterator operator--() {
+      this->_Ptr = _Acc::_Prev(this->_Ptr);
+      return *this;
+    }
+    iterator operator--(int) {
+      const_iterator _Tmp = *this;
+      --*this;
+      return _Tmp;
+    }
+    bool operator==(const iterator &_X) const { return this->_Ptr == _X._Ptr; }
+    bool operator!=(const iterator &_X) const { return !(*this == _X); }
+  };
+
  public:
-  explicit list() : _Head(_Buynode()), _Size(0) {
-
+  explicit list() : _Head(_Buynode()), _Size(0) {}
+  explicit list(size_type _N, const _Ty &_V = _Ty()) : _Head(_Buynode()), _Size(0) { insert(begin(), _N, _V); }
+  list(const _Ty *_F, const _Ty *_L) : _Head(_Buynode()), _Size(0) { insert(begin(), _F, _L); }
+  list(_It _F, _It _L) : _Head(_Buynode()), _Size(0) { insert(begin(), _F, _L); }
+  ~list() {
+    erase(begin(), end());
+    _Freenode(_Head);
+    _Head = 0;
+    _Size = 0;
   }
-  void push_back(const _Ty &v) {
 
-    _Nodeptr _S = _Buynode(_Head, _Acc::_Prev(_Head));
-    _S->_Value = v;
+ public:
+  size_type size() const { return _Size; }
+  bool empty() const { return (size() == 0); }
+  reference front() { return *begin(); }
+  reference back() { return *--end(); }
+  void push_back(const _Ty &v) { insert(end(), v); }
+  void push_front(const _Ty &v) { insert(begin(), v); }
+  void pop_back() { erase(--end()); }
+  void pop_front() { erase(begin()); }
+  
+//  iterator begin() { return iterator(_Acc::_Next(_Head)); }
+  iterator begin() const {
+    return iterator(_Acc::_Next(_Head));
+  }
+//  iterator end() { return iterator(_Head); }
+  iterator end() const {
+    return iterator(_Head);
+  }
+ public:
+  iterator insert(iterator _P, const _Ty &_X = _Ty()) {
+    _Nodeptr _S = _P._Mynode();
+    _Acc::_Prev(_S) = _Buynode(_S, _Acc::_Prev(_S));
+    _S = _Acc::_Prev(_S);
     _Acc::_Next(_Acc::_Prev(_S)) = _S;
-    _Acc::_Prev(_Head) = _S;
-    _Size++;
 
+    /////////////////////////
+    _Acc::_Value(_S) = _X;
+    //allocator.construct(&_Acc::_Value(_S), _X);
+    /////////////////////////
+    _Size++;
+    return iterator(_S);
   }
+
+  void insert(iterator _P, size_type _M, const _Ty &_X) {
+    for (; 0 < _M; --_M)
+      insert(_P, _X);
+  }
+  void insert(iterator _P, const _Ty *_F, const _Ty *_L) {
+    for (; _F != _L; ++_F)
+      insert(_P, *_F);
+  }
+  void insert(iterator _P, _It _F, _It _L) {
+    for (; _F != _L; ++_F)
+      insert(_P, *_F);
+  }
+
+  iterator erase(iterator _P) {
+    _Nodeptr _S = (_P++)._Mynode();
+    _Acc::_Next(_Acc::_Prev(_S)) = _Acc::_Next(_S);
+    _Acc::_Prev(_Acc::_Next(_S)) = _Acc::_Prev(_S);
+
+    /////////////////////
+    free(_S);
+    // allocator.destroy(&_Acc::_Value(_S)); //destroy object
+    _Freenode(_S);
+    /////////////////////
+    --_Size;
+    return _P;
+  }
+
+  iterator erase(iterator _F, iterator _L) {
+    while (_F != _L)
+      erase(_F++);
+    return _F;
+  }
+  void clear() { erase(begin(), end()); }
 
  protected:
   //_Narg  next argument  _Parg prev argument
-
   _Nodeptr _Buynode(_Nodeptr _Narg = 0, _Nodeptr _Parg = 0) {
     //_Nodeptr _S = (_Nodeptr) malloc(sizeof(_Node));
     _Nodeptr _S = (_Nodeptr) malloc(sizeof(_Node));
@@ -60,7 +196,6 @@ class list {
     _Acc::_Prev(_S) = _Parg != 0 ? _Parg : _S;
     return _S;
   }
-
  private:
   _Nodeptr _Head;
   size_type _Size;
--- my_stl/test_list.cpp
+++ my_stl/test_list.cpp
@@ -7,7 +7,30 @@ using namespace std;
 int main() {
   list<int> mylist;
   int arr[5] = {1, 2, 3, 4, 5};
-  for (int i = 0; i < 5; i++) {
-    mylist.push_back(i);
+//  for (int i = 0; i < 5; i++) {
+//    mylist.push_back(i);
+//  }
+//
+//  list<int>::iterator it = mylist.begin();
+//  while (it != mylist.end()) {
+//    cout << *it << " ";
+//    ++it;
+//  }
+//  cout << endl;
+
+  mylist.insert(mylist.end(), 10, 2);
+  list<int>::iterator it = mylist.begin();
+  while (it != mylist.end()) {
+    cout << *it << " ";
+    it++; //it++
   }
+  mylist.insert(mylist.end(), arr, arr + 5);
+  it = mylist.begin();
+  while (it != mylist.end()) {
+    cout << *it << " ";
+    it++; //it++
+  }
+
+  //list<int> youlist(10, 1);
+
 }
