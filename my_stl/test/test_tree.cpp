4ee66d03a2be8ee678ef56bd5ed49d5ba888f097
huichuan <lhc14124908@163.com>
2022-06-12 23:20:02 +0800

    add

--- my_stl/test/test_tree.cpp
+++ my_stl/test/test_tree.cpp
@@ -3,12 +3,36 @@
 //
 
 #include "../tree.h"
-
+#include "../xmemory.h"
 template<class _Ty>
 class KeyOfValue {
  public:
   const _Ty &operator()(const _Ty &_X) const { return _X; }
 };
 int main() {
-  _Tree<int, int, KeyOfValue<int>, less < int>, allocator < int > > tr(less<int>(), false);
+//  _Tree<int, int, KeyOfValue<int>, less<int>, allocator<int> > tr(less<int>(), false);
+
+  int ar[] = {10, 10, 10, 7, 8, 15, 5, 6, 11, 13, 12};
+  int n = sizeof(ar) / sizeof(int);
+  //_Tree<int, int, KeyOfValue<int>, less<int>, allocator<int> > tr(less<int>(),true);
+  _Tree<int, int, KeyOfValue<int>, less<int>, myallocator<int> > tr(less<int>(), false);
+  for (int i = 0; i < n; ++i)
+    tr.insert(ar[i]); //
+
+  _Tree<int, int, KeyOfValue<int>, less<int>, myallocator<int> >::iterator it = tr.begin();
+//  it++;
+//  it++;
+//  it++;
+//  it++;
+//  it++;
+
+//  cout << *it << endl;
+//  tr.erase(it);
+
+  while (it != tr.end()) {
+    cout << *it << " ";
+    ++it; //it++
+  }
+  cout << endl;
+  return 0 ;
 }
--- my_stl/tree.h
+++ my_stl/tree.h
@@ -5,37 +5,14 @@
 #ifndef PLAY_WITH_ALGO_MY_STL_TREE_H_
 #define PLAY_WITH_ALGO_MY_STL_TREE_H_
 #include "xmemory.h"
+#include <iostream>
 
+using namespace std;
 //set  map  multiset  multimap
-template<class _K, class _Ty, class _Kfn, class _Pr, class _A>
+template<class _K, class _Ty, class _Kfn, class _Pr, class _A=myallocator<_Ty>()>
 class _Tree {
-  enum _Redbl { _Red, _Black };
-
-  struct _Node;
-  typedef _Node *_Nodeptr;
-  struct _Node {
-    _Nodeptr _Left, _Parent, _Right;
-    _Ty _Value;
-    _Redbl _Color;
-  };
-  class iterator;
-  friend class iterator;
-
-  typedef _Node *&_Nodepref;
-  typedef const _K &_Keyref;
-  typedef _Redbl &_Rbref;
-  typedef _Ty &_Vref;
-  typedef pair<iterator, bool> _Pairib;
-  typedef pair<iterator, iterator> _Pairii;
-
-  static _Rbref _Color(_Nodeptr _P) { return ((_Rbref) (*_P)._Color); }
-  static _Keyref _Key(_Nodeptr _P) { return (_Kfn()(_Value(_P))); }
-  static _Nodepref _Left(_Nodeptr _P) { return ((_Nodepref) (*_P)._Left); }
-  static _Nodepref _Parent(_Nodeptr _P) { return ((_Nodepref) (*_P)._Parent); }
-  static _Nodepref _Right(_Nodeptr _P) { return ((_Nodepref) (*_P)._Right); }
-  static _Vref _Value(_Nodeptr _P) { return ((_Vref) (*_P)._Value); }
-
  public:
+  enum _Redbl { _Red, _Black };
   typedef _Tree<_K, _Ty, _Kfn, _Pr, _A> _Myt;
   typedef _K key_type;
   typedef _Ty value_type;
@@ -45,11 +22,14 @@ class _Tree {
   typedef const _Ty *const_pointer;
   typedef _Ty &reference;
   typedef const _Ty &const_reference;
- public:
-  explicit _Tree(const _Pr &_Parg, bool _Marg = true, const _A &_Al = _A())
-      : allocator(_Al), key_compare(_Parg), _Multi(_Marg) {
-    _Init();
-  }
+
+  struct _Node;
+  typedef _Node *_Nodeptr;
+  struct _Node {
+    _Nodeptr _Left, _Parent, _Right;
+    _Ty _Value;
+    _Redbl _Color;
+  };
   class iterator {
    public:
     iterator() {}
@@ -107,6 +87,27 @@ class _Tree {
    protected:
     _Nodeptr _Ptr;
   };
+  friend class iterator;
+
+  typedef _Node *&_Nodepref;
+  typedef const _K &_Keyref;
+  typedef _Redbl &_Rbref;
+  typedef _Ty &_Vref;
+  typedef pair<iterator, bool> _Pairib;
+  typedef pair<iterator, iterator> _Pairii;
+
+  static _Rbref _Color(_Nodeptr _P) { return ((_Rbref) (*_P)._Color); }
+  static _Keyref _Key(_Nodeptr _P) { return (_Kfn()(_Value(_P))); }
+  static _Nodepref _Left(_Nodeptr _P) { return ((_Nodepref) (*_P)._Left); }
+  static _Nodepref _Parent(_Nodeptr _P) { return ((_Nodepref) (*_P)._Parent); }
+  static _Nodepref _Right(_Nodeptr _P) { return ((_Nodepref) (*_P)._Right); }
+  static _Vref _Value(_Nodeptr _P) { return ((_Vref) (*_P)._Value); }
+
+ public:
+  explicit _Tree(const _Pr &_Parg, bool _Marg = true, const _A &_Al = _A())
+      : allocator(_Al), key_compare(_Parg), _Multi(_Marg) {
+    _Init();
+  }
 
   iterator begin() { return iterator(_Lmost()); }
   iterator end() { return iterator(_Head); }
@@ -136,6 +137,124 @@ class _Tree {
       return (_Pairib(_Insert(_X, _Y, _V), true));
     return (_Pairib(_P, false));
   }
+
+  iterator erase(iterator _P) {
+    _Nodeptr _X;
+    _Nodeptr _Y = (_P++)._Mynode();
+    _Nodeptr _Z = _Y;
+    if (_Left(_Y) == _Nil)
+      _X = _Right(_Y);
+    else if (_Right(_Y) == _Nil)
+      _X = _Left(_Y);
+    else
+      _Y = _Min(_Right(_Y)), _X = _Right(_Y);
+
+    if (_Y != _Z) {
+      _Parent(_Left(_Z)) = _Y;
+      _Left(_Y) = _Left(_Z);
+      if (_Y == _Right(_Z))
+        _Parent(_X) = _Y;
+      else {
+        _Parent(_X) = _Parent(_Y);
+        _Left(_Parent(_Y)) = _X;
+        _Right(_Y) = _Right(_Z);
+        _Parent(_Right(_Z)) = _Y;
+      }
+      if (_Root() == _Z)
+        _Root() = _Y;
+      else if (_Left(_Parent(_Z)) == _Z)
+        _Left(_Parent(_Z)) = _Y;
+      else
+        _Right(_Parent(_Z)) = _Y;
+      _Parent(_Y) = _Parent(_Z);
+      std::swap(_Color(_Y), _Color(_Z));
+      _Y = _Z;
+    } else {
+      _Parent(_X) = _Parent(_Y);
+      if (_Root() == _Z)
+        _Root() = _X;
+      else if (_Left(_Parent(_Z)) == _Z)
+        _Left(_Parent(_Z)) = _X;
+      else
+        _Right(_Parent(_Z)) = _X;
+
+      if (_Lmost() != _Z);
+      else if (_Right(_Z) == _Nil)
+        _Lmost() = _Parent(_Z);
+      else
+        _Lmost() = _Min(_X);
+      if (_Rmost() != _Z);
+      else if (_Left(_Z) == _Nil)
+        _Rmost() = _Parent(_Z);
+      else
+        _Rmost() = _Max(_X);
+    }
+    ////////////////////////////////////////////////////////////
+    //
+
+    if (_Color(_Y) == _Black) {
+      while (_X != _Root() && _Color(_X) == _Black) {
+        if (_X == _Left(_Parent(_X)))  //×ó·ÖÖ§
+        {
+          _Nodeptr _W = _Right(_Parent(_X));
+          if (_Color(_W) == _Red) {
+            _Color(_W) = _Black;
+            _Color(_Parent(_X)) = _Red;
+            _Lrotate(_Parent(_X));
+            _W = _Right(_Parent(_X));
+          }
+          if (_Color(_Left(_W)) == _Black && _Color(_Right(_W)) == _Black) {
+            _Color(_W) = _Red;
+            _X = _Parent(_X);
+          } else {
+            if (_Color(_Right(_W)) == _Black) {
+              _Color(_Left(_W)) = _Black;
+              _Color(_W) = _Red;
+              _Rrotate(_W);
+              _W = _Right(_Parent(_X));
+            }
+            _Color(_W) = _Color(_Parent(_X));
+            _Color(_Parent(_X)) = _Black;
+            _Color(_Right(_W)) = _Black;
+            _Lrotate(_Parent(_X));
+            break;
+          }
+        } else   //ÓÒ·ÖÖ§
+        {
+          _Nodeptr _W = _Left(_Parent(_X));
+          if (_Color(_W) == _Red) {
+            _Color(_W) = _Black;
+            _Color(_Parent(_X)) = _Red;
+            _Rrotate(_Parent(_X));
+            _W = _Left(_Parent(_X));
+          }
+          if (_Color(_Right(_W)) == _Black
+              && _Color(_Left(_W)) == _Black) {
+            _Color(_W) = _Red;
+            _X = _Parent(_X);
+          } else {
+            if (_Color(_Left(_W)) == _Black) {
+              _Color(_Right(_W)) = _Black;
+              _Color(_W) = _Red;
+              _Lrotate(_W);
+              _W = _Left(_Parent(_X));
+            }
+            _Color(_W) = _Color(_Parent(_X));
+            _Color(_Parent(_X)) = _Black;
+            _Color(_Left(_W)) = _Black;
+            _Rrotate(_Parent(_X));
+            break;
+          }
+        }
+      }
+      _Color(_X) = _Black;
+    }
+
+    _Destval(&_Value(_Y));
+    _Freenode(_Y);
+    --_Size;
+    return (_P);
+  }
  protected:
   _Nodeptr _Buynode(_Nodeptr _Parg, _Redbl _Carg) {
     _Nodeptr _S = (_Nodeptr) allocator._Charalloc(1 * sizeof(_Node));
@@ -143,8 +262,8 @@ class _Tree {
     _Color(_S) = _Carg;
     return (_S);
   }
-  void _Consval(pointer _P, const _Ty &_V) { _Construct(&*_P, _V); }
-  void _Destval(pointer _P) { _Destroy(&*_P); }
+  void _Consval(pointer _P, const _Ty &_V) { allocator.construct(&*_P, _V); }
+  void _Destval(pointer _P) { allocator.destroy(&*_P); }
   void _Freenode(_Nodeptr _S) { allocator.deallocate(_S, 1); }
   static _Nodeptr _Nil;  //1
   static size_t _Nilrefs;
